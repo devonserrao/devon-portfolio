b03ed624b3d6315b35d338ca38b07571daa64623
devonserrao <devonserrao101@gmail.com>
2021-08-18 19:30:13 -0500

    Added angular card template card functional component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
+import AngularCarCompProject from './components/AngularCarCompProject';
 import './App.css';
 
+function GreetingToPortfolio(props) {
+  return <h1> Welcome to {props.name}'s Portfolio!</h1>
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GreetingToPortfolio name="Devon Serrao" />
+      <AngularCarCompProject />
+
     </div>
   );
 }
